001202bc3f2c05dbd08e3be4fc5a05870737d158
notacoder-notyet <olegkliko@yandex.ru>
2022-05-12 18:38:05 +0300

    co

--- DZ_05.05/app.py
+++ DZ_05.05/app.py
@@ -1,31 +1,22 @@
+from datetime import datetime
+
 # from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy.sql import func
+# from sqlalchemy.orm import relationship, backref
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///db.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://test:test@localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+# load_dotenv()
 
 db = SQLAlchemy(app)
-# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-# if os.path.exists(dotenv_path):
-#     load_dotenv(dotenv_path)
-# app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('SQLALCHEMY_DATABASE_URI')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 
-class Member(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    create_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
-
-    def __repr__(self) -> str:
-        return f'<Member {self.firstname}>'
-
+from models import Member, Event, event_member
 
 @app.route('/')
 def index():
@@ -33,7 +24,7 @@ def index():
     return render_template('index.html', members=members)
 
 
-@app.route('/аpply', methods=('GET', 'POST'))
+@app.route('/аpply-to-<int:event_id>', methods=('GET', 'POST'), endpoint='apply')
 def apply():
     if request.method == 'POST':
         firstname = request.form['firstname']
@@ -44,7 +35,11 @@ def apply():
                           lastname=lastname,
                           email=email,
                           age=age)
+        # eventname = request.form['event']            
         db.session.add(member)
+        # event = db.session.query(Event).filter(Event.event_name == eventname).all()
+        
+        # event.membership.append(member)
         db.session.commit()
         return redirect(url_for('index'))
 
---
+++ DZ_05.05/models.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db = SQLAlchemy(app)
+
+
+event_member = db.Table('event_member', 
+    db.Column('member_id', db.Integer, db.ForeignKey('members.id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('events.id'))
+)
+
+
+class Member(db.Model):
+    __tablename__ = 'members'
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    membership = db.relationship("Event", secondary=event_member, backref='memberships')
+
+    def __repr__(self) -> str:
+        return f'<Member {self.firstname}>'
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(100), nullable=False)
+    event_info = db.Column(db.String(300), nullable=False)
+    event_date = db.Column(db.DateTime)
+
+    def __repr__(self) -> str:
+        return f'<Event {self.event_name}>'
+
+# db.create_all() #была тут только для однократного создания таблицы, т.к. в консоли плевалась ошибками
+# oleg = Member(firstname='Oleg', lastname='Klykov', age=20, email='oleg@mail.ru')
+# neoleg = Member(firstname='neOleg', lastname='neKlykov', age=21, email='neOleg@mail.ru')
+# test = Member(firstname='Test', lastname='Test', age=100, email='test@mail.ru')
+# event1 = Event(event_name='Event-1', event_info='Some Event-1')
+# event2 = Event(event_name='Event-2', event_info='Some Event-2')
+# db.session.add_all([oleg, neoleg, test, event1, event2])
+# # eventname = 'Event-1'
+# event = db.session.query(Event).filter(Event.event_name == 'Event-1').all()
+# print(event)
+# # event.membership.append(oleg)
+# db.session.commit()
